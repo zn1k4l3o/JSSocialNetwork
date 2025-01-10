4ffddd40dbadf3d088827b22b55389a0f7ec2d4e
zn1k4l3o <leo.znika@tvz.hr>
2025-01-10 13:08:12 +0100

    feat: added posts

--- social-network/src/app/app.module.ts
+++ social-network/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { MessageViewComponent } from './message-view/message-view.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    PostPageComponent,
+    UserPageComponent,
+    MessageViewComponent,
+    NavbarComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
---
+++ social-network/src/app/message-view/message-view.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MessageViewComponent } from './message-view.component';
+
+describe('MessageViewComponent', () => {
+  let component: MessageViewComponent;
+  let fixture: ComponentFixture<MessageViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [MessageViewComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MessageViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
